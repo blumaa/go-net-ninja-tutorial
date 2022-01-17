987775d0b5f82cd099a98fe013d9c05beba76123
Aaron Blum <a.blum@x-ion.de>
2022-01-17 17:14:00 +0100

    reverted to one main file

--- main.go
+++ main.go
@@ -2,35 +2,5 @@ package main
 
 func main() {
 
-	// simple loop
-	// x := 0
-
-	// for x < 5 {
-	// 	fmt.Println("the value of x is:", x)
-	// 	x++
-	// }
-
-	// while loop
-
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("the value of x is:", i)
-	// }
 	
-	// names := []string{"john", "ringo", "paul", "george"}
-	
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-	
-	// for index, value := range names{
-	// 	fmt.Printf("the value at index %v is %v \n", index, value)
-	// }
-
-	// for _, value := range names{
-	// 	fmt.Printf("the value is %v \n", value)
-	// 	value = "new string"
-	// }
-
-	// fmt.Println(names)
-
 }
