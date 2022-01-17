5ccbfb65eab88f7a4bd005608f28be3ba8cbc722
Aaron Blum <a.blum@x-ion.de>
2022-01-17 17:12:55 +0100

    final

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
+	}
+
+	names := []string{"john", "paul", "george", "ringo"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at position", index)
+			break
+		}
+		fmt.Printf("the value at pos %v is %v \n", index, value)
+	}
 }
