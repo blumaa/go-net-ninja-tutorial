cae6e53def355ea4502ad07cb3f76668dc0ce722
Aaron Blum <a.blum@x-ion.de>
2022-01-20 11:22:56 +0100

    updated functions

--- main.go
+++ main.go
@@ -31,11 +31,11 @@ func main() {
 	cycleNames([]string{"cloud", "tifa", "barret"}, sayGreeting)
 	cycleNames([]string{"cloud", "tifa", "barret"}, sayBye)
 
-	a1 := circleArea(10.5)
-	a2 := circleArea(15)
+  //	a1 := circleArea(10.5)
+  //	a2 := circleArea(15)
 
-	fmt.Println(a1, a2)
-	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
+  //	fmt.Println(a1, a2)
+  //	fmt.Printf("circle 1 is %0.3f and circle 2 is %0.3f", a1, a2)
 	
 	
 }
